2a50d462076e1bc0dcba62d161fd2b2492968012
tsiklaurii <tsiklaurinino321@gmail.com>
2025-11-20 18:54:43 +0400

    refactor
    code refactor

--- rockstar-games-redesign/src/components/LandingPage/LandingPage.tsx
+++ rockstar-games-redesign/src/components/LandingPage/LandingPage.tsx
@@ -1,20 +1,22 @@
 import Header from "../Header/Header"
-
-import banner from '../../assets/images/GTA_6_banner.svg'
+import LatestNews from "../LatestNews/LatestNews"
 import Footer from "../Footer/Footer"
 import FeaturedGames from "../FeaturedGames/FeaturedGames"
 
+import banner from '../../assets/images/GTA_6_banner.svg'
 import GTA_5 from '../../assets/images/game_posters/GTA_5.svg'
 import GTA_online from '../../assets/images/game_posters/GTA_online.svg'
 import RED from '../../assets/images/game_posters/RED.svg'
 import RED_online from '../../assets/images/game_posters/RED_Online.svg'
+import GTA_6 from '../../assets/images/game_posters/GTA_6.svg'
 
 const LandingPage = () => {
     return (
         <div className="bg-[#101010] font-helvetica justify-items-center">
             <Header />
             <img src={banner} alt="banner" className="w-full" />
-            <FeaturedGames gamePoster1={GTA_5} gamePoster2={GTA_online} gamePoster3={RED} gamePoster4={RED_online}/>
+            <FeaturedGames gamePoster1={GTA_5} gamePoster2={GTA_online} gamePoster3={RED} gamePoster4={RED_online} />
+            <LatestNews gamePoster1={GTA_6} />
             <Footer />
         </div>
     )
