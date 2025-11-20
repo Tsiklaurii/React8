8e430f67804386ae87fb1a667e97ab71c4ac43a7
tsiklaurii <tsiklaurinino321@gmail.com>
2025-11-20 20:44:59 +0400

    fix/button
    fix button component

--- rockstar-games-redesign/src/components/Button/Button.tsx
+++ rockstar-games-redesign/src/components/Button/Button.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
 
 const Button = ({ btnText, className = "" }: ButtonProps) => {
     return (
-        <button className={`bg-[#FCAF17] hover:bg-[#fca017c5] transition-colors rounded-lg cursor-pointer text-[20px] ${className}`}>{btnText}</button>
+        <button className={`bg-[#FCAF17] hover:bg-[#fca017c5] duration-300 transition-colors rounded-lg cursor-pointer text-[20px] ${className}`}>{btnText}</button>
     );
 };
 
