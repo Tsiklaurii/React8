302bb60c454f842af8c99d41b96321da25e7f137
tsiklaurii <tsiklaurinino321@gmail.com>
2025-11-20 16:14:03 +0400

    feature/footer
    added footer

--- rockstar-games-redesign/src/components/LandingPage/LandingPage.tsx
+++ rockstar-games-redesign/src/components/LandingPage/LandingPage.tsx
@@ -1,12 +1,14 @@
 import Header from "../Header/Header"
 
 import banner from '../../assets/images/GTA_6_banner.svg'
+import Footer from "../Footer/Footer"
 
 const LandingPage = () => {
     return (
         <div>
             <Header />
             <img src={banner} alt="banner" className="w-full" />
+            <Footer />
         </div>
     )
 }
